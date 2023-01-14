2cb76eb03453be5e617e4e64873afb008cfd98b2
伊吹英晃 <ibuki-90@hotmail.co.jp>
2023-01-14 20:57:00 +0900

    update

--- common.sh
+++ common.sh
@@ -2,6 +2,3 @@
 ROOT_USER_PASSWORD='$6$7b32mBZF09wEjCUg$VgyWex/KZbCI/4E0r33KoKnT81oONPF/d7XPjn5jg9GCSnwPpnf0/aMyajMZTnzmtJ4ei18SVy4XtMOiI0Yol/'
 CREATE_NORMAL_USER=admin  ## Add the username here to create a user, leave empty to skip creating one
 NORMAL_USER_PASSWORD='$6$7b32mBZF09wEjCUg$VgyWex/KZbCI/4E0r33KoKnT81oONPF/d7XPjn5jg9GCSnwPpnf0/aMyajMZTnzmtJ4ei18SVy4XtMOiI0Yol/'
-
-## K3s configuration
-MASTER_NODE_ADDR='master1.local'  ## The ip or FQDN of the first node
