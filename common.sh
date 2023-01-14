db84096143b1c14117c141135678aba64f8e314d
伊吹英晃 <ibuki-90@hotmail.co.jp>
2023-01-14 18:12:58 +0900

    update

--- common.sh
+++ common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ROOT_USER_PASSWORD='$6$L9XlAcbPMHtOD4Q7$ruA1AHd4FLQ6pWlxVm7ay2yYrB7ZAzE3xJdE1EbuD9/IFv5YG/PTBBQpDWoQ0obhIq3n0Q2uUl8GR6RrJ2aBC/'
+ROOT_USER_PASSWORD='$6$7b32mBZF09wEjCUg$VgyWex/KZbCI/4E0r33KoKnT81oONPF/d7XPjn5jg9GCSnwPpnf0/aMyajMZTnzmtJ4ei18SVy4XtMOiI0Yol/'
 CREATE_NORMAL_USER=admin  ## Add the username here to create a user, leave empty to skip creating one
-NORMAL_USER_PASSWORD='$6$L9XlAcbPMHtOD4Q7$ruA1AHd4FLQ6pWlxVm7ay2yYrB7ZAzE3xJdE1EbuD9/IFv5YG/PTBBQpDWoQ0obhIq3n0Q2uUl8GR6RrJ2aBC/'
+NORMAL_USER_PASSWORD='$6$7b32mBZF09wEjCUg$VgyWex/KZbCI/4E0r33KoKnT81oONPF/d7XPjn5jg9GCSnwPpnf0/aMyajMZTnzmtJ4ei18SVy4XtMOiI0Yol/'
 
 ## K3s configuration
 MASTER_NODE_ADDR='master1.local'  ## The ip or FQDN of the first node
--- master3.sh
+++ master3.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-INSTALL_K3S_EXEC='server --server https://master1.local:6443 --cluster-cidr=10.42.0.0/16,2001:cafe:42:0::/56 --service-cidr=10.43.0.0/16,2001:cafe:42:1::/112'
-hRkb73Kvh
+INSTALL_K3S_EXEC="server --server https://$MASTER_NODE_ADDR:6443--cluster-cidr=10.42.0.0/16,2001:cafe:42:0::/56 --service-cidr=10.43.0.0/16,2001:cafe:42:1::/112"
